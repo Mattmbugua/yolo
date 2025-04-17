24d09c9329fb71b2f2490df027b47cbf4235b868
Mattmbugua <mathewmbugua2015@gmail.com>
2025-04-17 19:03:45 +0300

    please

--- backend/upload.js
+++ backend/upload.js
@@ -35,7 +35,7 @@ function checkFileType(file, cb){
     if(mimetype && extname){
         return cb(null, true);
     }else{
-        cb('Error: Images Only!!')
+        cb('Error: Images Only Please!!')
     }
 }
 
